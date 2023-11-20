42c33ceb4452aa697644d0f32583c1908e62ed24
Robotin Rares <robotin.r.a@gmail.com>
2023-11-20 21:45:50 +0200

    extract js in new file

---
+++ examples.js
@@ -0,0 +1,21 @@
+function variablesExamples() {
+  console.info(2 + 3);
+  console.warn("rares");
+
+  var employed = true;
+  var age = 18;
+  console.log("Angajat:", employed);
+  employed = false;
+  console.log("Angajat:", employed);
+  employed = "da";
+  console.log("Angajat:", employed);
+
+  var skills = ["html", "css"];
+  console.debug("tipul variabilei skills:", typeof skills);
+
+  var job = document.getElementById("job-title");
+  console.warn("job", job);
+  console.info(typeof job);
+}
+
+variablesExamples();
