cd622d606837b5bad08bc025d16bd569a7a497e2
Robotin Rares <robotin.r.a@gmail.com>
2023-12-04 18:59:21 +0200

    JS homework

---
+++ Homework/Teme.js
@@ -0,0 +1,45 @@
+//Cub 1/2
+function changeBlue() {
+  document.getElementById("red").style.backgroundColor = "red";
+  document.getElementById("blue").style.backgroundColor = "blue";
+}
+function changeRed() {
+  document.getElementById("blue").style.backgroundColor = "red";
+  document.getElementById("red").style.backgroundColor = "blue";
+}
+
+//Cub 3/6
+function changeOrange() {
+  document.getElementById("orange").style.backgroundColor = "orange";
+  document.getElementById("turquoise").style.backgroundColor = "turquoise";
+}
+function changeTurquoise() {
+  document.getElementById("turquoise").style.backgroundColor = "orange";
+  document.getElementById("orange").style.backgroundColor = "turquoise";
+}
+
+//Cub 4/5
+function changeOrangered() {
+  document.getElementById("orangered").style.backgroundColor = "orangered";
+  document.getElementById("purple").style.backgroundColor = "purple";
+}
+function changePurple() {
+  document.getElementById("orangered").style.backgroundColor = "purple";
+  document.getElementById("purple").style.backgroundColor = "orangered";
+}
+
+//Cub 7/8
+function changeGreen() {
+  document.getElementById("green").style.backgroundColor = "green";
+  document.getElementById("yellow").style.backgroundColor = "yellow";
+}
+function changeYellow() {
+  document.getElementById("green").style.backgroundColor = "yellow";
+  document.getElementById("yellow").style.backgroundColor = "green";
+}
+
+//Cub 9
+function changeMagenta() {
+  document.getElementById("green,yellow,blue").style.backgroundColor =
+    "magenta";
+}
